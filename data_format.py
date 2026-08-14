98ff5fe418482ec4aa365950c63cd5068a7fc4e3
QNH1013 <156663715+NIKRIE0706@users.noreply.github.com>
2026-08-14 10:48:39 +0200

    Add initial data_format.py with sensor data structure

---
+++ data_format.py
@@ -0,0 +1,18 @@
+{
+    "timestamp": 12.481,
+    "pressure": 1008.3,
+    "temperature": 27.4,
+    "ax": 0.12,
+    "ay": -0.04,
+    "az": 9.71,
+    "gx": 0.03,
+    "gy": -0.01,
+    "gz": 0.07,
+    "gps": {
+        "latitude": null,
+        "longitude": null,
+        "altitude": null,
+        "speed": null,
+        "satellites": null
+    }
+}
